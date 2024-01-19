3be314657732856c56b6cd5d5658deae987c8657
Your Name <swifty2205@yahoo.co.uk>
2024-01-19 18:02:22 +0000

    Ruting functions delete and post completed

--- lesson-data.js
+++ lesson-data.js
@@ -1,14 +1,14 @@
 [
-    { "subject": "Mathematics", "location": "Manchester", "price": 80, "spaces": 5, "cart": 0, "image": "fa-calculator" },
-    { "subject": "English", "location": "London", "price": 100, "spaces": 5, "cart": 0, "image": "fa-book" },
-    { "subject": "Drama", "location": "London", "price": 90, "spaces": 5, "cart": 0, "image": "fa-theater-masks" },
-    { "subject": "Science", "location": "Bristol", "price": 70, "spaces": 5, "cart": 0, "image": "fa-flask" },
-    { "subject": "English", "location": "Birmingham", "price": 120, "spaces": 5, "cart": 0, "image": "fa-book" },
-    { "subject": "Music", "location": "Nottingham", "price": 110, "spaces": 5, "cart": 0, "image": "fa-music" },
-    { "subject": "History", "location": "Liverpool", "price": 90, "spaces": 5, "cart": 0, "image": "fa-history" },
-    { "subject": "Chess", "location": "Leeds", "price": 90, "spaces": 5, "cart": 0, "image": "fa-chess" },
-    { "subject": "English", "location": "Coventry", "price": 100, "spaces": 5, "cart": 0, "image": "fa-book" },
-    { "subject": "History", "location": "Liverpool", "price": 90, "spaces": 5, "cart": 0, "image": "fa-history" },
-    { "subject": "Drama", "location": "London", "price": 90, "spaces": 5, "cart": 0, "image": "fa-theater-masks" },
-    { "subject": "English", "location": "London", "price": 100, "spaces": 5, "cart": 0, "image": "fa-book" }
+    { "subject": "Mathematics", "location": "Manchester", "price": 80, "spaces": 5, "cart": 0, "icon": "fa-calculator" },
+    { "subject": "English", "location": "London", "price": 100, "spaces": 5, "cart": 0, "icon": "fa-book" },
+    { "subject": "Drama", "location": "London", "price": 90, "spaces": 5, "cart": 0, "iicon": "fa-theater-masks" },
+    { "subject": "Science", "location": "Bristol", "price": 70, "spaces": 5, "cart": 0, "icon": "fa-flask" },
+    { "subject": "English", "location": "Birmingham", "price": 120, "spaces": 5, "cart": 0, "icon": "fa-book" },
+    { "subject": "Music", "location": "Nottingham", "price": 110, "spaces": 5, "cart": 0, "icon": "fa-music" },
+    { "subject": "History", "location": "Liverpool", "price": 90, "spaces": 5, "cart": 0, "icon": "fa-history" },
+    { "subject": "Chess", "location": "Leeds", "price": 90, "spaces": 5, "cart": 0, "icon": "fa-chess" },
+    { "subject": "English", "location": "Coventry", "price": 100, "spaces": 5, "cart": 0, "icon": "fa-book" },
+    { "subject": "History", "location": "Liverpool", "price": 90, "spaces": 5, "cart": 0, "icon": "fa-history" },
+    { "subject": "Drama", "location": "London", "price": 90, "spaces": 5, "cart": 0, "icon": "fa-theater-masks" },
+    { "subject": "English", "location": "London", "price": 100, "spaces": 5, "cart": 0, "icon": "fa-book" }
 ]
