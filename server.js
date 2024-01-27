5036fb9d794bed96ec25cfbfde53ebdc5b1b7600
Your Name <swifty2205@yahoo.co.uk>
2024-01-27 03:51:42 +0000

    postman routes complete

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const { connectToMongoDB, getDB } = require('./mongoDB');
-const { ObjectId } = require('mongodb');
-const path = require('path');
-const fs = require('fs');
-const morgan = require('morgan');
+//modules and set up express app
+const express = require("express");
+const cors = require("cors");
+const { connectToMongoDB, getDB } = require("./mongoDB");
+const { ObjectId } = require("mongodb");
+const path = require("path");
+const fs = require("fs");
+const morgan = require("morgan");
 const port = process.env.PORT || 3000;
 
 
@@ -13,46 +14,48 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 //Console logger with short format
-app.use(morgan('short'));
+app.use(morgan("short"));
 
-app.use('/public/images', function (req, res, next) {
-    const imagePath = path.join(__dirname, 'public/images', req.url);
+// Serving static images from the 'public/images' directory
+app.use("/public/images", function (req, res, next) {
+    const imagePath = path.join(__dirname, "public/images", req.url);
 
     // Check if the image file exists
     fs.access(imagePath, fs.constants.F_OK, function (err) {
         if (err) {
 
-            console.error('Image' + req.url + 'not found');
-            res.status(404).send('Image not found');
+            console.error("Image" + req.url + "not found");
+            res.status(404).send("Image not found");
         } else {
-            // if Image exists, serve it
-            express.static(path.join(__dirname, 'public/images'))(req, res, next);
+            // if Image exists serve it
+            express.static(path.join(__dirname, "public/images"))(req, res, next);
         }
     });
 });
 
 // Static files from the 'School_Class_App' directory
-app.use(express.static(path.join(__dirname, '..', 'School_Class_App',)));
+app.use(express.static(path.join(__dirname, "..", "School_Class_App",)));
 
 
 
 // Middleware to handle dynamic collection names
-app.param('collectionName', function (req, res, next, collectionName) {
+app.param("collectionName", function (req, res, next, collectionName) {
     req.collection = getDB().collection(collectionName);
     return next();
 });
 
-app.get('/search/collections/:collectionName/:query', (req, res) => {
-   
+// Search route based on subject or location in a specific collection
+app.get("/search/collections/:collectionName/:query", (req, res) => {
+
     const collectionName = req.params.collectionName;
     req.collection = getDB().collection(collectionName);
-    console.log('collectionName:', collectionName);
+    console.log("collectionName:", collectionName);
 
     const searchTerm = req.params.query;
 
-   
+    // Performing a search in the products based on subject or location
     if (!req.collection) {
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: "Internal Server Error" });
     }
 
     req.collection.find({
@@ -62,8 +65,8 @@ app.get('/search/collections/:collectionName/:query', (req, res) => {
         ]
     }).toArray((err, searchResults) => {
         if (err) {
-            console.error('Error during search:', err);
-            return res.status(500).json({ error: 'Internal Server Error' });
+            console.error("Error during search:", err);
+            return res.status(500).json({ error: "Internal Server Error" });
         }
 
         res.json(searchResults);
@@ -71,10 +74,10 @@ app.get('/search/collections/:collectionName/:query', (req, res) => {
 });
 
 
-
-app.get('/collections/:collectionName'
+// Get all documents from a chosen collection
+app.get("/collections/:collectionName"
     , function (req, res, next) {
-        console.log('Request received for collection:', req.params.collectionName);
+        console.log("Request received for collection:", req.params.collectionName);
         req.collection.find({}).toArray(function (err, results) {
             if (err) {
                 return next(err);
@@ -84,37 +87,9 @@ app.get('/collections/:collectionName'
         });
     });
 
-// Example route setup
-app.get('/collections/products/search/:query', (req, res) => {
-    const searchTerm = req.params.query;
-    console.log('req.collection:', req.collection);
 
-    // Make sure req.collection is properly set using the middleware or other means.
-    if (!req.collection) {
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    req.collection.find({
-        $text: { $search: searchTerm }
-    }).toArray((err, searchResults) => {
-        if (err) {
-            console.error('Error during search:', err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
-
-        res.json(searchResults);
-    });
-});
-
-
-
-
-
-
-
-
-
-app.get('/collections/:collectionName/:id'
+// Get a specific document from a collection by its ID
+app.get("/collections/:collectionName/:id"
     , function (req, res, next) {
         req.collection.findOne({ _id: new ObjectId(req.params.id) }, function (err, results) {
             if (err) {
@@ -123,9 +98,11 @@ app.get('/collections/:collectionName/:id'
             res.send(results);
         });
     });
-app.post('/collections/:collectionName', function (req, res, next) {
-    const collectionName = req.params.collectionName;
+// Insert a new document into a specific collection
+app.post("/collections/:collectionName", function (req, res, next) {
+
     const newElement = req.body;
+    const collectionName = req.params.collectionName;
 
     req.collection = getDB().collection(collectionName);
 
@@ -139,8 +116,8 @@ app.post('/collections/:collectionName', function (req, res, next) {
 });
 
 
-
-app.delete('/collections/:collectionName/:id'
+// Delete a specific document from a collection by its ID
+app.delete("/collections/:collectionName/:id"
     , function (req, res, next) {
         req.collection.deleteOne({ _id: new ObjectId(req.params.id) }, function (err, results) {
             if (err) {
@@ -152,15 +129,14 @@ app.delete('/collections/:collectionName/:id'
 
 
 // If the lessonIDs and spaces are provided,filter based on lesson IDs and
-//update spaces field in the products collection.
-// Handle updating available spaces in the products table
-app.put('/collections/:collectionName', function (req, res, next) {
+// update spaces field in the products collection.
+app.put("/collections/:collectionName", function (req, res, next) {
     const lessonIDs = req.body.lessonIDs;
     const spaces = req.body.spaces;
 
     if (lessonIDs !== undefined && spaces !== undefined) {
 
-        req.collection.updateOne(
+        req.collection.updateMany(
             { lesson_id: { $in: lessonIDs } },
             { $set: { spaces: parseInt(spaces) } },
             function (err, result) {
@@ -175,28 +151,19 @@ app.put('/collections/:collectionName', function (req, res, next) {
         res.json({ message: "Invalid lesson IDs or spaces provided for update" });
     }
 });
-// app.get('/collections/products',  function (req, res, next) {
-//     try {
-//       console.log('Request received for collection:', req.params.collectionName);
-//       const results =  req.collection.find({}).toArray();
-//       res.send(results);
-//       console.log(results);
-//     } catch (error) {
-//       console.error('Error in /collections/:collectionName:', error);
-//       res.status(500).send('Internal Server Error');
-//     }
-//   });
 
 
 
 
+// Route to Handle 404 error for unspecified routes
 app.use(function (req, res) {
     res.status(404).send("File not found")
 });
 
+// Connect to MongoDB and start the server
 connectToMongoDB().then(() => {
     app.listen(port, function (err) {
-        if (err) console.log('Error connecting to port.')
-        console.log('Server is listening on port http://localhost:' + port)
+        if (err) console.log("Error connecting to port.")
+        console.log("Server is listening on port http://localhost:" + port)
     })
 });
